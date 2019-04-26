8604e84223dfe68601533b35552244e615a4ca95
Elias Sells <esell002@ucr.edu>
2019-04-25 19:48:39 -0700

    Jumping and Gravity
    Added Jumping and improved Gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
 export var PLAYER_SPEED = 200
+export var PLAYER_JUMP_SPEED = -1800
 
 
 var RayNode
-var Player_Gravity = 100
+var Player_Gravity = 300
 var BoltCooldown = 0
+var jumping = false
+var LongJump = false
+var FastFall = false
+var JumpVelocity = 0
 var CurrSprite
 var CurrCollision
 
@@ -33,18 +38,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (RayNode.is_colliding()):
-		print("reeee")
 	var motion = Vector2()
 	var GravityMotion = Vector2(0, Player_Gravity)
-	
 	if (Input.is_action_pressed("ui_shoot")):
 		if (BoltCooldown <= 0):
 			CreateLightning()
 			BoltCooldown = 60
-	if (Input.is_action_pressed("ui_up")):
-		#todo
-		pass
+	if (Input.is_action_pressed("ui_up") and !jumping and test_move(get_transform(), Vector2(0,10))):
+		jumping = true
+		LongJump = true
+		JumpVelocity = PLAYER_JUMP_SPEED
 	if (Input.is_action_pressed("ui_right")):
 		motion += Vector2(1, 0)
 		RayNode.set_rotation_degrees(0)
@@ -63,21 +66,45 @@ func _process(delta):
 		#CurrCollision.scale = Vector2(1,1)
 		#move_and_collide(Vector2(0,-20))
 		pass
+	if (jumping and Input.is_action_pressed("ui_up") and LongJump):
+		JumpVelocity += 40
+	elif (jumping and Input.is_action_just_released("ui_up") and LongJump):
+		JumpVelocity += 100
+		LongJump = false
+		FastFall = true
+	elif (FastFall):
+		JumpVelocity += 130
+		if (test_move(get_transform(), Vector2(0,10))):
+			FastFall = false
+			jumping = false
+			JumpVelocity = 0
 	BoltCooldown -= 1
-	motion = motion.normalized()*PLAYER_SPEED*delta
-	GravityMotion *= delta
-	move_and_collide(motion)
-	move_and_collide(GravityMotion)
+	motion[0] = motion[0]*PLAYER_SPEED#*delta
+	if (test_move(get_transform(), Vector2(0,10))):
+		Player_Gravity = 300
+	else:
+		Player_Gravity += 40
+	if (jumping):
+		motion[1] += JumpVelocity#*delta
+	#GravityMotion *= delta
+	motion[1] += GravityMotion[1]
+	#print(motion[0])
+	#print(motion[1])
+	move_and_slide(motion)
 
 func CreateLightning():
-	var Lightning = load("res://Assets/Lightning.tscn")
+	var Lightning = load("res://Scenes/Lightning.tscn")
 	var LightningInstance = Lightning.instance()
 	LightningInstance.set_name("bolt")
 	LightningInstance.get_node("LightningRotation").set_rotation_degrees(get_node("Rotation").get_rotation_degrees())
 	var CurrPos = get_position()
 	var LightningPos = Vector2()
 	LightningPos[0] = CurrPos[0] + (200 * cos(LightningInstance.get_node("LightningRotation").get_rotation_degrees()))
-	LightningPos[1] = CurrPos[1] #+ (200 * sin(LightningInstance.get_node("LightningRotation").get_rotation_degrees()))
+	LightningPos[1] = CurrPos[1] - 100#+ (200 * sin(LightningInstance.get_node("LightningRotation").get_rotation_degrees()))
 	LightningInstance.set_position(LightningPos)
 	get_node("/root").add_child(LightningInstance)
+	
+	
+	
+	
 	

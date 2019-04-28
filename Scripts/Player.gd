89f438995ece8711565a9a679843deb9f1472e22
Charlie <cchin013@ucr.edu>
2019-04-27 22:57:41 -0700

    Overriding melissa's changes with Elias's because it's been 20 years wak
    e up you've been asleep for so long

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,39 +1,5 @@
 extends KinematicBody2D
 
-<<<<<<< HEAD
-# Declare member variables here. Examples:
-
-export var motion_speed = 140
-
-var rayNode
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	set_physics_process(true)
-	rayNode = get_node("Raycast")
-	
-func _physics_process(delta):
-	var motion = Vector2()
-	
-	if(Input.is_action_pressed("ui_up")):
-		motion += Vector2(0, -1)
-		rayNode.rotation = 180
-		
-	if(Input.is_action_pressed("ui_down")):
-		motion += Vector2(0, 1)
-		rayNode.rotation = 0
-		
-	if(Input.is_action_pressed("ui_left")):
-		motion += Vector2(-1, 0)
-		rayNode.rotation = -90
-
-	if(Input.is_action_pressed("ui_right")):
-		motion += Vector2(1, 0)
-		rayNode.rotation = 90
-		
-	motion =  motion.normalized() * motion_speed * delta
-	move_and_collide(motion)
-=======
 #Static Variables
 export var PLAYER_SPEED = 200
 export var PLAYER_JUMP_SPEED = -1800
@@ -161,9 +127,3 @@ func SpawnMeleeHitbox():
 	MeleeHitPos[1] -= 85
 	MeleeHitInstance.set_position(MeleeHitPos)
 	get_node("/root").add_child(MeleeHitInstance)
-	
-	
-	
-	
-	
->>>>>>> playerbehavior

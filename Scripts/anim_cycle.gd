8e9926ffee4b2f661849cfbd14f7ad1a5e693943
Melissa Santos <msant035@ucr.edu>
2019-05-01 21:38:24 -0700

    Added enemies to demo

--- Scripts/anim_cycle.gd
+++ Scripts/anim_cycle.gd
@@ -9,20 +9,20 @@ var timer
 func _ready():
 	var sprite = self.find_node(sprite_name)
 	animator = sprite.get_child(0)
-	anim_list = animator.get_animation_list()
-	timer = Timer.new()
-	
-	add_child(timer)
-	timer.set_wait_time(animator.current_animation_length)
-	timer.connect("timeout", self, "_cycle")
-	timer.start()
-	
-func _cycle():
-	if (cur_anim < anim_list.size()- 1):
-		cur_anim = cur_anim + 1
-	else:
-		cur_anim = 0
-		
-	animator.play(anim_list[cur_anim])
-	timer.set_wait_time(animator.current_animation_length-0.1)
-	
+
+#	anim_list = animator.get_animation_list()
+#	timer = Timer.new()
+#
+#	add_child(timer)
+#	timer.set_wait_time(animator.current_animation_length)
+#	timer.connect("timeout", self, "_cycle")
+#	timer.start()
+#
+#func _cycle():
+#	if (cur_anim < anim_list.size()- 1):
+#		cur_anim = cur_anim + 1
+#	else:
+#		cur_anim = 0
+#
+#	animator.play(anim_list[cur_anim])
+#	timer.set_wait_time(animator.current_animation_length-0.1)

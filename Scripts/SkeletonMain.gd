5e1fd3eaba045464e9bbaa39e745aa8995d87492
Joshua Pedron <jpedron12@gmail.com>
2019-05-23 06:01:17 -0700

    I fixed it except for the damage
    buy me a granola bar this time

--- Scripts/SkeletonMain.gd
+++ Scripts/SkeletonMain.gd
@@ -1,7 +1,7 @@
 extends "res://Scripts/BaseEnemy.gd"
-
+ 
 #testtest
-
+ 
 #export var SkeletonHealth = 500
 export var SkeletonSpeed = 70
 var moving = false
@@ -13,99 +13,131 @@ onready var CurrSprite = get_node("skeleton")
 onready var Animator = CurrSprite.get_node("AnimationPlayer")
 var StaggerCounter = 0
 var DeathCounter = 0
-
-
-
+var DetectAttack = false
+var AttackDetection = 30
+var AttackTimer = 0
+var Attacking = false
+ 
+ 
+ 
 func _ready():
-	set_process(true)
-	
+    set_process(true)
+   
 func _process(delta):
-	#Enemy Detection
-	DistToPlayer = get_global_transform()[2] - (Player.get_global_transform()[2])
-	if (abs(DistToPlayer[0]) < MaxDetection):
-		#print(rad2deg(acos(DistToPlayer.normalized().dot(Facing))))
-		if (rad2deg(acos(DistToPlayer.normalized().dot(Facing))) - 100 > FOV):
-			DetectPlayer = true
-			if (abs(DistToPlayer[0]) < AttackDetection and not Attacking):
-				AttackTimer = 120
-				Attacking = true
-		else:
-			DetectPlayer = false
-	else:
-		DetectPlayer = false
-	#print(DetectAttack)
-	var Direction = SkeletonRandomMovement(delta)
-	var motion = Vector2()
-	var GravityMotion = Vector2(0, Skeleton_Gravity)
-	#moving = false
-	if (DeathCounter > 0):
-		DeathCounter -= 1
-		print(DeathCounter)
-		if (DeathCounter == 0):
-			print("hello")
-			queue_free()
-		return
-	if (StaggerCounter > 0):
-		StaggerCounter -= 1
-		return
-	if (Direction == "left"):
-		motion = Vector2(-1,0)
-		CurrSprite.flip_h = true
-		Animator.play("walk")
-	elif (Direction == "right"):
-		motion = Vector2(1,0)
-		CurrSprite.flip_h = false
-		Animator.play("walk")
-	elif (Direction == "none"):
-		Animator.play("idle")
-		pass
-
-	if (test_move(get_transform(), Vector2(0,0.1))):
-		Skeleton_Gravity = 100
-	else:
-		Skeleton_Gravity += 12
-	motion[0] *= SkeletonSpeed
-	motion[1] += GravityMotion[1]
-	move_and_slide(motion)
-	
-	
+    #Enemy Detection
+    DistToPlayer = get_global_transform()[2] - (Player.get_global_transform()[2])
+    if (abs(DistToPlayer[0]) < MaxDetection):
+        #print(rad2deg(acos(DistToPlayer.normalized().dot(Facing))))
+        if (rad2deg(acos(DistToPlayer.normalized().dot(Facing))) - 135 > FOV):
+            DetectPlayer = true
+            if (abs(DistToPlayer[0]) < AttackDetection and not Attacking):
+                AttackTimer = 120
+                Attacking = true
+        else:
+            DetectPlayer = false
+    else:
+        DetectPlayer = false
+    #print(DetectAttack)
+    var Direction = SkeletonRandomMovement(delta)
+    var motion = Vector2()
+    var GravityMotion = Vector2(0, Skeleton_Gravity)
+    #moving = false
+    if (DeathCounter > 0):
+        DeathCounter -= 1
+        AttackTimer = 0
+        Attacking = false
+        if (DeathCounter == 0):
+            queue_free()
+        return
+    if (StaggerCounter > 0):
+        StaggerCounter -= 1
+        AttackTimer = 0
+        Attacking = false
+        return
+    if (AttackTimer >= 0):
+        AttackTimer -= 1
+        Animator.play("attack")
+        if (AttackTimer == 68):
+            SkeletonAttack()
+        if (AttackTimer <= 0):
+            Attacking = false
+        return
+    if (DetectPlayer and Facing[0] == -1):
+            motion = Vector2(-1,0)
+            CurrSprite.flip_h = true
+            Animator.play("walk")
+    elif (DetectPlayer and Facing[0] == 1):
+            motion = Vector2(1,0)
+            CurrSprite.flip_h = false
+            Animator.play("walk")
+    else:
+        if (Direction == "left"):
+            motion = Vector2(-1,0)
+            Facing = Vector2(-1,0)
+            CurrSprite.flip_h = true
+            Animator.play("walk")
+        elif (Direction == "right"):
+            motion = Vector2(1,0)
+            Facing = Vector2(1,0)
+            CurrSprite.flip_h = false
+            Animator.play("walk")
+        elif (Direction == "none"):
+            Animator.play("idle")
+            pass
+ 
+    if (test_move(get_transform(), Vector2(0,0.1))):
+        Skeleton_Gravity = 100
+    else:
+        Skeleton_Gravity += 12
+    motion[0] *= SkeletonSpeed
+    motion[1] += GravityMotion[1]
+    move_and_slide(motion)
+   
+   
 func SkeletonRandomMovement(delta):
-	if (LeftMove > 0):
-		LeftMove -= 1
-		return "left"
-	if (RightMove > 0):
-		RightMove -= 1
-		return "right"
-
-		
-	if (RandomMoveCounter > 240):
-		var temp = randi()%2 #can be expanded to add things like cool idle animations
-		var transform = get_transform()
-		if (temp == 0):
-			transform[2][0] += -65*SkeletonSpeed*delta
-		if (temp == 1):
-			transform[2][0] += 65*SkeletonSpeed*delta
-		if (temp == 0 and (test_move(transform, Vector2(0,2)))): #left
-			LeftMove = 65
-			RandomMoveCounter = 0
-		elif (temp == 1 and (test_move(transform, Vector2(0,2)))): #right
-			RightMove = 65
-			RandomMoveCounter = 0
-	RandomMoveCounter += 1
-	return "none"
-
+    if (LeftMove > 0):
+        LeftMove -= 1
+        return "left"
+    if (RightMove > 0):
+        RightMove -= 1
+        return "right"
+ 
+       
+    if (RandomMoveCounter > 240):
+        var temp = randi()%2 #can be expanded to add things like cool idle animations
+        var transform = get_transform()
+        if (temp == 0):
+            transform[2][0] += -65*SkeletonSpeed*delta
+        if (temp == 1):
+            transform[2][0] += 65*SkeletonSpeed*delta
+        if (temp == 0 and (test_move(transform, Vector2(0,2)))): #left
+            LeftMove = 65
+            RandomMoveCounter = 0
+        elif (temp == 1 and (test_move(transform, Vector2(0,2)))): #right
+            RightMove = 65
+            RandomMoveCounter = 0
+    RandomMoveCounter += 1
+    return "none"
+ 
 func Take_Damage(damage):
-	Health -= damage
-	StaggerCounter = 42
-	Animator.play("stagger")
-	var temp = Vector2()
-	temp = (Player.get_global_transform()[2]) - get_global_transform()[2]
-	if (temp[0] > 0):
-		Facing[0] = 1
-	else:
-		Facing[0] = -1
-	
+    Health -= damage
+    StaggerCounter = 42
+    Animator.play("stagger")
+   
 func Die():
-	Animator.play("dead")
-	DeathCounter = 90
-	
+    Animator.play("dead")
+    DeathCounter = 90
+ 
+ 
+func SkeletonAttack():
+    var MeleeHit = load("res://Scenes/SkeletonAttack.tscn")
+    var MeleeHitInstance = MeleeHit.instance()
+    MeleeHitInstance.set_name("melee")
+    var MeleeHitPos = get_position()
+    if (Facing[0] == -1):
+        MeleeHitPos[0] -= 24
+    if (Facing[0] == 1):
+        MeleeHitPos[0] += 24
+    MeleeHitPos[1] += 2
+    MeleeHitInstance.set_position(MeleeHitPos)
+    get_node("/root").add_child(MeleeHitInstance)

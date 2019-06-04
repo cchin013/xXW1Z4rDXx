e146acafd7c4cc9e1dde5b2294ce4a4f4a04ab88
Melissa Santos <msant035@ucr.edu>
2019-06-04 10:48:05 -0700

    Added GUI into full level

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -3,18 +3,15 @@ extends MarginContainer
 onready var number_label = $Bars/LifeBar/Count/Background/Number
 onready var bar = $Bars/LifeBar/Gauge
 onready var tween = $Tween
+var player_max_health = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player_max_health = $"../../Player".PlayerHealth
+	player_max_health = $"../../Player".PlayerHealth
 	bar.max_value = player_max_health
 	bar.value = player_max_health
 	number_label.text = str(player_max_health)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Player_healthChanged(value, revive=false):
 	update_health(value, revive)
@@ -23,7 +20,7 @@ func update_health(value, revive):
 	var new_value
 	
 	if revive:
-		new_value = value
+		new_value = player_max_health
 	else:
 		new_value = bar.value - value if ((bar.value - value) > 0) else 0
 		

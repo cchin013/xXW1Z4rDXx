903ca5e2a0a1c365d32c1a6434d3e8ee2c5e5a43
Elias Sells <esell002@ucr.edu>
2019-12-24 03:22:20 -0800

    some player animation changes
    crouching is mostly fixed (still needs a crouch walk animation), crouch slashing added, animation logic should be more robust

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -24,7 +24,9 @@ var MeleeTimer = 0
 
 var is_grounded
 var jumping = false
-var moving = false
+var fullstop = false
+var crouching
+var DoAnimationLogic = true
 var hasSpell = {"lightning" : true, "fire" : true, "earth" : true, "water" : true}
 var currentSpell = "lightning"
 var playerSize
@@ -33,7 +35,8 @@ var IFrames = 0
 var Dying = false
 var StaggerCounter = 0
 var DeathCounter = 0
-var RayNode
+var CrouchCounter = 0
+var CrouchAttackCounter = 0
 var CurrSprite
 var CurrCollision
 var startPos
@@ -42,7 +45,10 @@ var ManaRegenCount = 30
 var AttackAnimationTimer = 0
 var Raycaster
 
-var motion = Vector2()
+
+var walk_direction = 0
+var velocity = Vector2()
+var facing = 1
 
 var Animator
 
@@ -65,10 +71,8 @@ var UP = Vector2(0, -1)
 ##Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
-	RayNode = $Rotation
 	CurrSprite = $PlayerSprite
 	CurrCollision = $PlayerCollision
-	RayNode.set_rotation_degrees(-90)
 	Animator = CurrSprite.get_node("general") # animation player
 	playerSize = CurrCollision.get_shape().get_extents()
 	startPos = get_global_transform()
@@ -80,11 +84,15 @@ func _ready():
 
 ##Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#motion = Vector2.ZERO
 	var collide_obj_id = 0
-	moving = false
-	var count = Raycaster.raycast(Raycaster.DOWN, CurrCollision.shape, collision_mask).count
-	var is_grounded = true if (count > 0) else false
+	
+	#checks if player is on the grounded
+	var collision_count = Raycaster.raycast(Raycaster.DOWN, CurrCollision.shape, collision_mask).count
+	var is_grounded = true if (collision_count > 0) else false
+	
+	#checks if player's head is hitting ceiling
+	var collision_count2 = Raycaster.raycast(Raycaster.UP, CurrCollision.shape, collision_mask).count
+	var hitting_ceiling = true if (collision_count2 > 0) else false
 
 	
 	if (Input.is_action_just_pressed("ui_revive") and Dying):
@@ -105,20 +113,42 @@ func _process(delta):
 	if (DeathCounter > 0 or Dying):
 		DeathCounter -= 1
 		DisableInput = true
-		motion[0] = 0
+		fullstop = true
+		DoAnimationLogic = false
 
 	elif (StaggerCounter > 0):
 		StaggerCounter -= 1
 		DisableInput = true
-		motion[0] = 0
-
+		fullstop = true
+		DoAnimationLogic = false
+	elif (crouching):
+		if (CrouchAttackCounter > 0):
+			fullstop = true
+			DisableInput = true
+			Animator.play("crouch_slash")
+			CrouchAttackCounter -= 1
+			if (CrouchAttackCounter == 18):
+				SpawnMeleeHitbox()
+			if (CrouchAttackCounter == 0):
+				Animator.play("crouch")
+				Animator.seek(0.3, true)
+		elif (CrouchCounter < 0):
+			Animator.stop()
+		else:
+			Animator.play("crouch")
+			CrouchCounter -= 1
+		DoAnimationLogic = false
 	elif (AttackAnimationTimer > 0):
-		motion[0] = 0
-		Animator.play("attack")
+		fullstop = true
 		DisableInput = true
+		DoAnimationLogic = false
+		Animator.play("attack")
 		AttackAnimationTimer -= 1
 		if (AttackAnimationTimer == 30):
 			SpawnMeleeHitbox()
+	
+	if (Input.is_action_pressed("quit")):
+			get_tree().quit()
 	
 	#Spell Wheel
 	if (Input.is_action_pressed("ui_selectFire") && hasSpell["fire"]):
@@ -143,86 +173,113 @@ func _process(delta):
 					
 		##Punching
 		if (Input.is_action_just_pressed("ui_melee") and MeleeTimer <= 0):
-			MeleeTimer = 1
-			AttackAnimationTimer = 40
-			#SpawnMeleeHitbox()
+			if (crouching):
+				CrouchAttackCounter = 24
+				MeleeTimer = .45
+			else:
+				AttackAnimationTimer = 40
+				MeleeTimer = .7
+
 			
 		if (is_grounded):
 			pass
 			
 		##Jumping
-		#print(motion[1])
-		#print(Input.is_action_pressed("ui_up"))
 		if (Input.is_action_pressed("ui_up") and !jumping and is_grounded):
-			motion[1] = MaxJumpSpeed
+			velocity[1] = MaxJumpSpeed
 			jumping = true
 			
-		if (!(Input.is_action_pressed("ui_up")) and jumping and motion[1] < MinJumpSpeed):
-			motion[1] = MinJumpSpeed
+		if (!(Input.is_action_pressed("ui_up")) and jumping and velocity[1] < MinJumpSpeed):
+			velocity[1] = MinJumpSpeed
 		
 		if !(Input.is_action_pressed("ui_up")):
 			jumping = false
+		
+		if (hitting_ceiling and velocity[1] < MinJumpSpeed + 200):
+			velocity[1] = MinJumpSpeed + 201
+		
 
 		
 		##Left/Right Movement
-		
-		if (Input.is_action_pressed("ui_right")):
-			motion[0] = 1
-			RayNode.set_rotation_degrees(-90)
-			CurrSprite.flip_h = false
-			Animator.play("run")
-			moving = true
-		elif (Input.is_action_pressed("ui_left")):
-			motion[0] = -1
-			RayNode.set_rotation_degrees(90)
-			CurrSprite.flip_h = true
-			Animator.play("run")
-			moving = true
+		if (Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left")):
+			walk_direction = 1
+			facing = 1
+			#Animator.play("run")
+		elif (Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right")):
+			walk_direction = -1
+			facing = -1
+			#Animator.play("run")
 		else:
-			motion[0] = 0
-		##Crouch, TODO
-		if (Input.is_action_pressed("ui_down")):
-			Animator.play("crouch")
-			moving = true
-			pass
-		else:
-			pass
+			walk_direction = 0
+			
+		##Crouch, NEED TO FIX ANIMATION WHILE WALKING AND CROUCHING
+		if (Input.is_action_pressed("ui_down") and !crouching):
+			CrouchCounter= 15
+			crouching = true
+		elif (!Input.is_action_pressed("ui_down")):
+			CrouchCounter = 0
+			crouching = false
+
 	#End General Input Block	
+	
+	if (facing == 1):
+		CurrSprite.flip_h = false
+	else:
+		CurrSprite.flip_h = true
 		
 	#Melee Hitbox Timing
 	if (MeleeTimer >= 0):
 		MeleeTimer -= 1*delta
 			
+	#Mana Regen
 	if (ManaRegenCount <= 0):
 		if (PlayerMana < 100):
 			PlayerMana += 1
 			emit_signal("manaChanged", 1)
 		ManaRegenCount = 30
 	ManaRegenCount -= 1
+	
 	##Ticks Down Bolt Cooldown
 	BoltCooldown -= 2*delta
 
-	##Finalizes motion vector and moves character
-	motion[0] = motion[0]*PLAYER_SPEED
-	motion[1] += Gravity*delta
-	#Maximum Fall Speed
-	if (is_grounded):
-		motion[1] = clamp(motion[1], -2000, 100)
+	##Applies player speed and gravity to motion vector
+	if (fullstop):
+		velocity[0] = 0
 	else:
-		motion[1] = clamp(motion[1], -2000, 450)
+		velocity[0] = lerp(velocity[0], walk_direction*PLAYER_SPEED, 0.11)
+	if (abs(velocity[0]) < 10 and walk_direction == 0):
+		velocity[0] = 0
 		
-	move_and_slide(motion, UP)
+	velocity[1] += Gravity*delta
 	
-#	if get_slide_count() > 0:
-#		collide_obj_id = get_slide_collision(get_slide_count() - 1)
-#		if collide_obj_id.collider.name == "PlatformV" or collide_obj_id.collider.name == "PlatformH":
-#			self.set_sync_to_physics(true)
-#		else:
-#			self.set_sync_to_physics(false)
+	if (DoAnimationLogic):
+		if (velocity[1] < 0 and !is_grounded):
+			if (AttackAnimationTimer > 0):
+				Animator.play("jump_attack")
+			else:
+				Animator.play("jump")
+		elif (velocity[1] > 0 and !is_grounded):
+			if (AttackAnimationTimer > 0):
+				Animator.play("jump_attack")
+			else:
+				Animator.play("fall")
+		elif (velocity[0] != 0):
+			Animator.play("run")
+		else:
+			Animator.play("idle")
 	
-	if(!moving and !DisableInput):
-		Animator.play("idle")
+	#Clamps y velocity 
+	if (is_grounded):
+		velocity[1] = clamp(velocity[1], -1000, 100)
+	else:
+		velocity[1] = clamp(velocity[1], -1000, 450)
+	
+	#Moves the character
+	move_and_slide(velocity, UP)
+	
 	DisableInput = false
+	fullstop = false
+	DoAnimationLogic = true
 
 ##Spawns Spell Bolt
 func CreateBolt():
@@ -256,11 +313,12 @@ func CreateBolt():
 	
 	var BoltInstance = Bolt.instance()
 	BoltInstance.set_name("bolt")
-	BoltInstance.get_node(currRotation).set_rotation_degrees(get_node("Rotation").get_rotation_degrees())
 	var BoltPos = get_position()
-	if (RayNode.get_rotation_degrees() == -90):
+	if (facing == -1):
+		BoltInstance.get_node(currRotation).set_rotation_degrees(90)
 		BoltPos[0] += 8
-	if (RayNode.get_rotation_degrees() == 90):
+	if (facing == 1):
+		BoltInstance.get_node(currRotation).set_rotation_degrees(-90)
 		BoltPos[0] -= 8
 	BoltPos[1] -= 2
 	BoltInstance.set_position(BoltPos)
@@ -277,9 +335,9 @@ func SpawnMeleeHitbox():
 	var MeleeHitInstance = MeleeHit.instance()
 	MeleeHitInstance.set_name("melee")
 	var MeleeHitPos = get_position()
-	if (RayNode.get_rotation_degrees() == -90):
+	if (facing == 1):
 		MeleeHitPos[0] += 32
-	if (RayNode.get_rotation_degrees() == 90):
+	if (facing == -1):
 		MeleeHitPos[0] -= 32
 	MeleeHitPos[1] += 2
 	MeleeHitInstance.set_position(MeleeHitPos)
@@ -306,5 +364,5 @@ func Revive():
 	Dying = false
 	self.set_transform(startPos)
 
-func PlayAnimation():
+func AnimationLogic():
 	pass
